7e38ce703589b9ef0b1fa17b116de41ac2b57918
octocactuses <octocactuses@gmail.com>
2025-06-02 13:33:55 -0500

    structre neurons

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <vector>
+
+class Neuron{
+private:
+  int number_of_parents;
+  std::vector<Neuron*> parents;
+  std::vector<float> parent_weights;
+  float bias;
+  float activation;
+public:
+  Neuron(
+    int number_of_parents,
+    std::vector<Neuron*> parents,
+    std::vector<float> parent_weights,
+    float bias
+  ) {
+    this->bias = bias;
+    this->number_of_parents = number_of_parents;
+    this->parents = parents;
+    this->parent_weights = parent_weights;
+  }
+
+};
 
 int main() {
   std::cout << "hello world\n";
